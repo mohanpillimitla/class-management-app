d215cacaf75c24ed3d7056372b60ee3811fd7ff5
mohanpillimitla <mohanpillimitla143@gmail.com>
2021-07-05 00:44:54 +0530

    feat: routes added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,8 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   app.get(
     "/api/test/teacher",
-    [authJwt.verifyToken,authJwt.isTeacher],
+    [authJwt.verifyToken, authJwt.isTeacher],
     controller.teacherBoard
   );
 
